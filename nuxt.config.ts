11f56e775af0e2f9e463fd08b007511e22d54c30
tatsuyuki.yamada <aeon20250624@gmail.com>
2026-04-20 22:29:02 +0900

    FOR [Duplicated imports useAppConfig,,,,]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@pinia/nuxt',
-  ],
-  css: ['~/assets/css/mybulma.css'],
-  build: {
-    transpile: ['@vuepic/vue-datepicker']
-  },
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    modules: [
+        '@nuxt/content',
+        '@pinia/nuxt',
+    ],
+    css: ['~/assets/css/mybulma.css'],
+    build: {
+        transpile: ['@vuepic/vue-datepicker']
+    },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+    experimental: {
+        serverAppConfig: false,
+    },
 })
